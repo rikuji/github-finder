296fc46a2a466b1c90f1c2ef46c325bdcf0b4c56
lucas.valenzuela <lucas.valenzuela@squadra.com.br>
2020-07-25 09:39:00 -0300

    L#16

---
+++ src/components/users/Search.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends Component {
+  state = {
+    text: '',
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div>
+        <form className='form' onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            onChange={this.onChange}
+            value={this.state.text}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
