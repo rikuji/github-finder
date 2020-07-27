80261a25c5e2c155f0a1fa1bff51ce81619e57df
lucas.valenzuela <lucas.valenzuela@squadra.com.br>
2020-07-27 09:06:36 -0300

    L#28

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ setAlert, searchUsers, showClear, clearUsers }) => {
+const Search = ({ setAlert, showClear, clearUsers }) => {
+  const githubContext = useContext(GithubContext);
+
   const [text, setText] = useState('');
 
   const onSubmit = (e) => {
@@ -9,7 +12,7 @@ const Search = ({ setAlert, searchUsers, showClear, clearUsers }) => {
     if (text === '') {
       setAlert('Please enter something', 'light');
     } else {
-      searchUsers(text);
+      githubContext.searchUsers(text);
       setText('');
     }
   };
@@ -42,7 +45,6 @@ const Search = ({ setAlert, searchUsers, showClear, clearUsers }) => {
 };
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired,
